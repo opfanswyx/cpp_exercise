4d213985e1589e743dcdb8358f562f24af90eda9
Atticus <opfanswyx@outlook.com>
2019-12-01 21:21:29 +0800

    c++ primer base

--- C++Primer/chapter3/main.cpp
+++ C++Primer/chapter3/main.cpp
@@ -32,7 +32,7 @@ int main()
 }
 #endif
 
-#if 1 //3.3
+#if 0 //3.4
 
 #include <iostream>
 #include <string>
@@ -40,6 +40,96 @@ int main()
 using namespace std;
 int main()
 {
+    string s1, s2;
+    cout<<"please input two sting:"<<endl;
+    cin >> s1 >> s2;
+    if(s1 == s2)
+    {
+        cout << "equal" << endl;
+    }
+    else if(s1 > s2)
+    {
+        cout << "s1 long" << endl;
+    }
+    else{
+        cout << "s2 long" << endl;
+    }
 
+    if(s1.size() == s2.size())
+    {
+        cout << "equal" << endl;
+    }
+    else if(s1.size() > s2.size())
+    {
+        cout << "s1 long" << endl;
+    }
+    else
+    {
+        cout << "s2 long" << endl;
+    }
+    return 0;
 }
+#endif
+
+#if 0 // 3.5
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char cont = 'y';
+    string s, result;
+    cout<<"please input the first string:"<<endl;
+    while(cin>>s)
+    {
+        result += s;
+        cout << "continue?(y or n)"<< endl;
+        cin >> cont;
+        if(cont == 'y' || cont == 'Y')
+            cout << "please input next string:"<<endl;
+        else
+            break;
+    }
+    cout << "thr result string is : "<<result<<endl;
+
+    return 0;
+}
+#endif
+
+#if 0 // 3.5
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char cont = 'y';
+    string s, result;
+    cout<<"please input the first string:"<<endl;
+    while(cin>>s)
+    {
+        if(!result.size())
+            result += s;
+        else
+            result = result + " " + s;
+        cout << "continue?(y or n)"<< endl;
+        cin >> cont;
+        if(cont == 'y' || cont == 'Y')
+            cout << "please input next string:"<<endl;
+        else
+            break;
+    }
+    cout << "thr result string is : "<<result<<endl;
+
+    return 0;
+}
+#endif
+
+#if 1 // 3.6
+
 #endif
